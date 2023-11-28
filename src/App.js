8b791005a6d0bc36e0fba0d338c694d36962b750
Juraj Bogdan Januš <ultra.master.mega@gmail.com>
2023-11-28 09:36:54 +0100

    Final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTaskForm from "./components/AddTaskForm";
 import TaskList from "./components/TaskList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (task) => {
     setTasks([...tasks, { text: task, completed: false }]);
@@ -27,10 +38,56 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <AddTaskForm addTask={addTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} toggleTask={toggleTask} />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>To-Do App</h1>
+        <nav>
+          <li>
+            <Link to="/">Svi Taskovi</Link>
+          </li>
+          <li>
+            <Link to="/active">Aktivni Taskovi</Link>
+          </li>
+          <li>
+            <Link to="/completed">Dovršeni Taskovi</Link>
+          </li>
+        </nav>
+        <AddTaskForm addTask={addTask} />
+        <Routes>
+          <Route
+            path="/active"
+            element={
+              <TaskList
+                tasks={tasks.filter((task) => !task.completed)}
+                deleteTask={deleteTask}
+                toggleTask={toggleTask}
+              />
+            }
+          />
+          <Route
+            path="/completed"
+            element={
+              <TaskList
+                tasks={tasks.filter((task) => task.completed)}
+                deleteTask={deleteTask}
+                toggleTask={toggleTask}
+              />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <TaskList
+                tasks={tasks}
+                deleteTask={deleteTask}
+                toggleTask={toggleTask}
+              />
+            }
+          />
+        </Routes>
+        <button onClick={() => setTasks([])}>Obriši sve Taskove</button>
+      </div>
+    </Router>
   );
 }
 
